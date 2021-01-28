a6933a6fa6a7d756a61384b9e5add04c0cd103f5
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-01-28 04:54:44 -0500

    se actualizo swagger a v3

--- src/main/java/com/idat/proyect/web/config/SwaggerConfig.java
+++ src/main/java/com/idat/proyect/web/config/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package com.idat.proyect.web.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -10,8 +12,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+     @Bean
      public Docket api() {
-          return new Docket(DocumentationType.SWAGGER_2).select()
-                    .apis(RequestHandlerSelectors.basePackage("com.idat.proyect.web.controllers")).build();
+          return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any()).build();
      }
+
 }
--- src/main/java/com/idat/proyect/web/security/SecurityConfig.java
+++ src/main/java/com/idat/proyect/web/security/SecurityConfig.java
@@ -61,7 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      @Override
      public void configure(WebSecurity web) throws Exception {
           web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
-                    "/configuration/security", "/swagger-ui.html", "/webjars/**");
+                    "/configuration/security", "/swagger-ui/**", "/webjars/**");
      }
 
      // le indicamos que implicitamente las configuraciones que realizemos las use
